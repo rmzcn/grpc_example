4768096c4cfb48fd24cafefcede6705d33d4ac35
ramazan.golgen <ramazan.golgen@piworks.net>
2021-09-04 16:36:15 +0300

    init

---
+++ GRPC Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Grpc.Core;
+using Helloworld;
+
+namespace GreeterClient
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:30051", ChannelCredentials.Insecure);
+
+            var client = new Greeter.GreeterClient(channel);
+            String user = "you";
+
+            var reply = client.SayHello(new HelloRequest { Name = user });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
